c3acdfb401cfccc91eb7cf75d43d05798c2728c5
wbr <1306688142@qq.com>
2022-08-05 15:27:06 +0800

    submit

---
+++ db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+
+mongoose
+  .connect("mongodb://localhost:27017/cat-shop", {
+    useNewUrlParser: true
+  })
+  .then(res => {
+    // console.log(res);
+    console.log("数据库连接成功");
+  })
+  .catch(err => {
+    console.log(err);
+  });
---
+++ models/index.js
@@ -0,0 +1,16 @@
+const User = require("./user");
+const Product = require("./product");
+const Manager = require("./manager");
+const Order = require("./order");
+const ProductCategory = require("./product_category");
+const UserAddress = require("./address");
+
+
+module.exports = {
+  User,
+  Product,
+  Manager,
+  Order,
+  ProductCategory,
+  UserAddress,
+};
